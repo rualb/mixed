4d4b2d12177859c269201507df48549ee5941261
Ru Al <39805456+rualb@users.noreply.github.com>
2018-09-26 11:52:47 +0400

    Update algorithm.java

--- algorithm.java
+++ algorithm.java
@@ -7,8 +7,55 @@ import java.util.*;
 
 public class Main {
 
+        public static void cut_rod_maximize_profit() {
+
+        int rodLen = 5;
+        int[] parts = {1, 2, 3, 4};
+        int[] part_price = {2, 5, 7, 8};
+
+        int[][] buf = new int[parts.length][rodLen + 1];
+
+
+        for (int p = 0; p < buf.length; ++p) {
+
+            int[] tots_for_parts = buf[p];
+
+            int part = parts[p];
+
+            for (int t = 0; t < tots_for_parts.length; ++t) {
+
+                if (t == 0) {
+                    //used as dummy
+                    tots_for_parts[t] = 0;
+                    continue;
+                }
+                if (part == 1) {
+                    tots_for_parts[t] = t * part_price[p];
+                    continue;
+                }
+
+                if (part > t) {
+                    //copy from prev
+                    tots_for_parts[t] = buf[p - 1][t] ;
+                    continue;
+                }
+                //buf[c-1][t]+buf[c][t-coin]
+                tots_for_parts[t] = Math.max(
+                        buf[p - 1][t],
+                        part_price[p] + buf[p][t - part]);//!!!!
+
+            }
+
+        }
+
+
+       // System.out.println(buf[parts.length - 1][rodLen]);
+
+
+    }
+
          
-          public static void coin_changing_minimum_number_of_coins() {
+        public static void coin_changing_minimum_number_of_coins() {
 
         int tot = 11;
         int[] cois = {1, 5, 6, 8};
